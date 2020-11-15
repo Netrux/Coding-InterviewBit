521af398438cb150df576bd50a895203595f6059
Nitin Chauhan <42533130+Netrux@users.noreply.github.com>
2020-11-15 11:05:37 +0530

    Create copy_list.cpp

---
+++ Hashing/copy_list.cpp
@@ -0,0 +1,41 @@
+RandomListNode* Solution::copyRandomList(RandomListNode* A) {
+    if (!A)
+        return NULL;
+    
+    RandomListNode* head = A;
+    
+    while (A)
+    {
+        RandomListNode* copyA = new RandomListNode(A->label);
+        copyA->next = A->next;
+        A->next = copyA;
+        A = copyA->next;
+    }
+    A = head;
+    
+    while (A)
+    {
+        if (!A->random)             // I missed this check.
+            A->next->random = NULL;
+        else
+            A->next->random = A->random->next;
+            
+        A = A->next->next;
+    }
+    A = head;
+    
+    RandomListNode* copy = A->next;
+    RandomListNode* copyHead = A->next;
+    
+    while (A)
+    {
+        A->next = A->next->next;
+        A = A->next;
+        if (!copy->next)            // I missed this check.
+            break;
+        copy->next = copy->next->next;
+        copy = copy->next;
+    }
+    
+    return copyHead;
+}
