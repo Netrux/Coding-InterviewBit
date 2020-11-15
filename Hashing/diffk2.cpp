82654448e0688d1c54d748c6ca9d5ec6a878a42c
Nitin Chauhan <42533130+Netrux@users.noreply.github.com>
2020-11-15 11:04:05 +0530

    Create diffk2.cpp

---
+++ Hashing/diffk2.cpp
@@ -0,0 +1,22 @@
+int Solution::diffPossible(const vector<int> &A, int B) {
+    unordered_map <int,int > mp;
+    for(int i = 0; i < A.size();i++){
+        if(mp.find(A[i]) == mp.end())
+            mp[A[i]] = 1;
+        else
+            mp[A[i]]++;
+    }
+    
+    for(int i = 0;i < A.size(); i++){
+        if(B == 0){
+            if(mp[A[i]] > 1)
+                return 1;
+        }
+        
+        else{
+            if(mp.find(A[i] + B) != mp.end())
+                return 1;
+        }
+    }
+    return 0;
+}
