aee990075d0f28f6d8155effd607348e223f5434
Nitin Chauhan <42533130+Netrux@users.noreply.github.com>
2020-11-21 11:04:29 +0530

    Create sorted_to_BST.cpp

---
+++ tree/sorted_to_BST.cpp
@@ -0,0 +1,20 @@
+TreeNode* makeBST(const vector<int> &A, int start, int end){
+    if(start > end){
+        return NULL;
+    }
+    int mid = start + (end - start)/2;
+    
+    TreeNode* root = new TreeNode(A[mid]);
+    
+    root->left = makeBST(A, start, mid-1);
+    
+    root->right = makeBST(A, mid+1, end);
+    
+    return root;
+}
+
+TreeNode* Solution::sortedArrayToBST(const vector<int> &A) {
+    
+    return makeBST(A, 0, A.size()-1);
+
+}
